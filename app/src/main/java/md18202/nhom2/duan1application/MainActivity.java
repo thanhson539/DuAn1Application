d701af952354b37239d12a22b034baead6992a07
thanhson539 <sonntph30566@fpt.edu.vn>
2023-06-27 22:52:06 +0700

    commit

---
+++ app/src/main/java/md18202/nhom2/duan1application/MainActivity.java
@@ -0,0 +1,14 @@
+package md18202.nhom2.duan1application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
