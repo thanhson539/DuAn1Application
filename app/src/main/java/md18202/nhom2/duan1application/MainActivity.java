84ac812567afb5c7ea44361811926586d7874b59
thanhson539 <sonntph30566@fpt.edu.vn>
2023-06-30 07:58:40 +0700

    giao dien man hinh chao

--- app/src/main/java/md18202/nhom2/duan1application/MainActivity.java
+++ app/src/main/java/md18202/nhom2/duan1application/MainActivity.java
@@ -3,6 +3,8 @@ package md18202.nhom2.duan1application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
