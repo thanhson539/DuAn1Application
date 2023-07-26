6a787b06d287e2cc6aab20308d7aadaf5d5ff444
thanhson539 <sonntph30566@fpt.edu.vn>
2023-07-26 18:48:16 +0700

    ok

---
+++ app/src/main/java/md18202/nhom2/duan1application/Activities/DonHangActivity.java
@@ -0,0 +1,16 @@
+package md18202.nhom2.duan1application.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import md18202.nhom2.duan1application.R;
+
+public class DonHangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_don_hang);
+    }
+}
