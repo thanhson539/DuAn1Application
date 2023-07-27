27f850f4ba8c1af89a7125729b6b59c582a7d837
thanhson539 <sonntph30566@fpt.edu.vn>
2023-07-27 14:44:42 +0700

    ok

--- app/src/main/java/md18202/nhom2/duan1application/Fragments/DonHang_Fragment.java
+++ app/src/main/java/md18202/nhom2/duan1application/Fragments/DonHang_Fragment.java
@@ -8,14 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 import md18202.nhom2.duan1application.R;
 
 public class DonHang_Fragment extends Fragment {
+    private TabLayout tabLayout_donhang;
+    private ViewPager2 viewPager_donhang;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_don_hang, container, false);
+        tabLayout_donhang = view.findViewById(R.id.tabLayout_donhang);
+        viewPager_donhang = view.findViewById(R.id.viewPager_donhang);
+
         return view;
     }
 }
