ee28d630b6c1ef6282a9dfda521b770280874591
thanhson539 <sonntph30566@fpt.edu.vn>
2023-07-27 23:48:17 +0700

    ok

--- app/src/main/java/md18202/nhom2/duan1application/DAO/HoaDonChiTietDAO.java
+++ app/src/main/java/md18202/nhom2/duan1application/DAO/HoaDonChiTietDAO.java
@@ -12,32 +12,12 @@ import md18202.nhom2.duan1application.Models.HoaDonChiTiet;
 
 public class HoaDonChiTietDAO {
     DBHelper dbHelper;
-    public HoaDonChiTietDAO(Context context){
+
+    public HoaDonChiTietDAO(Context context) {
         dbHelper = new DBHelper(context);
     }
 
-    public ArrayList<HoaDonChiTiet> getDsHoaDonChiTiet(){
-        ArrayList<HoaDonChiTiet> listResult = new ArrayList<>();
-        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery("select hdct.hoaDon_id as hoaDon_id, hdct.sanPham_id as sanPham_id," +
-                "hdct.trangThaiThanhToan , hdct.trangThaiDonHang  from HOADONCHITIET hdct " +
-                "inner join HOADON hd on hdct.hoaDon_id = hd.hoaDon_id", null);
-        if (cursor.getCount() != 0){
-            cursor.moveToFirst();
-            do {
-                listResult.add(new HoaDonChiTiet(
-                        cursor.getInt(0), //hoaDonChiTiet_id
-                        cursor.getInt(1), //hoaDon_id
-                        cursor.getInt(2), //sanPham_id
-                        cursor.getInt(3), //trangThaiDonHang
-                        cursor.getInt(4)  //trangThaiThanhToan
-                ));
-            }while (cursor.moveToNext());
-        }
-        return listResult;
-    }
-
-    public long themHoaDonChiTiet(HoaDonChiTiet hdct){
+    public long themHoaDonChiTiet(HoaDonChiTiet hdct) {
         SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("hoaDon_id", hdct.getHoaDon_id());
@@ -45,8 +25,39 @@ public class HoaDonChiTietDAO {
         values.put("soLuong", hdct.getSoLuong());
         values.put("trangThaiDonHang", hdct.getTrangThaiThanhToan());
         values.put("trangThaiThanhToan", hdct.getTrangThaiDonHang());
-
         return sqLiteDatabase.insert("HOADONCHITIET", null, values);
+    }
 
+    //Lấy danh sách đơn hàng thông qua hóa đơn chi tiết
+    public ArrayList<HoaDonChiTiet> getDonHangByHDCT(int trangThaiDonHang){
+        ArrayList<HoaDonChiTiet> listResult = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("" +
+                "select hdct.hoaDon_id, hdct.sanPham_id, hdct.soLuong, hdct.trangThaiDonHang, hdct.trangThaiThanhToan, " +
+                "sp.tenSanPham, sp.anhSanPham, sp.giaSanPham, " +
+                "hd.ngayMua, hd.diaChi, (sp.giaSanPham * hdct.soLuong) as tongTien " +
+                "from HOADONCHITIET hdct " +
+                "inner join HOADON hd on hdct.hoaDon_id = hd.hoaDon_id " +
+                "inner join SANPHAM sp on hdct.sanPham_id = sp.sanPham_id " +
+                "where hdct.trangThaiDonHang = ? ", new String[]{String.valueOf(trangThaiDonHang)});
+        if (cursor.getCount()!=0){
+            cursor.moveToFirst();
+            do {
+                listResult.add(new HoaDonChiTiet(
+                        cursor.getInt(0),   //hoaDon_id
+                        cursor.getInt(1),   //sanPham_id
+                        cursor.getInt(2),   //soLuong
+                        cursor.getInt(3),   //trangThaiDonHang
+                        cursor.getInt(4),   //trangThaiThanhToan
+                        cursor.getString(5),//tenSanPham
+                        cursor.getString(6),//anhSanPham
+                        cursor.getInt(7),   //giaSanPham
+                        cursor.getString(8),//ngayMua
+                        cursor.getString(9),//diaChi
+                        cursor.getInt(10)   //tongTien
+                ));
+            }while (cursor.moveToNext());
+        }
+        return listResult;
     }
 }
