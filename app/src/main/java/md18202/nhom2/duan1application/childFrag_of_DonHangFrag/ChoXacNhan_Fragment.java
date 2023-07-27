6a32d2112bdc9d18f4d3edcc1dcf316bad9f7c0d
thanhson539 <sonntph30566@fpt.edu.vn>
2023-07-28 01:21:23 +0700

    ok

--- app/src/main/java/md18202/nhom2/duan1application/childFrag_of_DonHangFrag/ChoXacNhan_Fragment.java
+++ app/src/main/java/md18202/nhom2/duan1application/childFrag_of_DonHangFrag/ChoXacNhan_Fragment.java
@@ -8,14 +8,34 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import md18202.nhom2.duan1application.Adapters.DonHangAdapter;
+import md18202.nhom2.duan1application.DAO.HoaDonChiTietDAO;
+import md18202.nhom2.duan1application.Models.HoaDonChiTiet;
 import md18202.nhom2.duan1application.R;
 
 public class ChoXacNhan_Fragment extends Fragment {
+    private RecyclerView ryc_choXacNhan;
+    private HoaDonChiTietDAO hoaDonChiTietDAO;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cho_xac_nhan, container, false);
+        ryc_choXacNhan = view.findViewById(R.id.ryc_choXacNhan);
+        loadData(ryc_choXacNhan);
         return view;
+    }
+    private void loadData(RecyclerView ryc_choXacNhan){
+        hoaDonChiTietDAO = new HoaDonChiTietDAO(getContext());
+        ArrayList<HoaDonChiTiet> list = hoaDonChiTietDAO.getDonHangByHDCT(0);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        ryc_choXacNhan.setLayoutManager(linearLayoutManager);
+        DonHangAdapter adapter = new DonHangAdapter(getContext(),list);
+        ryc_choXacNhan.setAdapter(adapter);
     }
 }
